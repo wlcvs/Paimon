53d546cdefb283dcda23f40e8b711746f7dec72e
Wallacy V. <wlcsv8080@outlook.com>
2024-12-15 15:53:47 -0300

    Falta mais organização

--- matriz/web/models.py
+++ matriz/web/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.exceptions import ValidationError
 
 class Matriz(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, null=True)
     num_of_lines_m = models.IntegerField(default=0)
     num_of_columns_n = models.IntegerField(default=0)
 
@@ -11,16 +11,24 @@ class Matriz(models.Model):
 
 class Element(models.Model):
     matriz = models.ForeignKey(Matriz, on_delete=models.CASCADE)
-    # value
+    value = models.IntegerField(default=0)
     line = models.IntegerField(default=0)
     column = models.IntegerField(default=0)
 
     def element_is_within_limits(self):
         if self.line > self.matriz.num_of_lines_m or self.column > self.matriz.num_of_columns_n:
             raise ValidationError("The element is outside the bounds of the matriz")
+        else:
+            return True
 
     # Verificar se o elemento já nesta posição já existe na matriz em questão
+    def there_is_already_an_element_in_this_position(self):
+        if  Matriz.objects.filter(line__exact=self.line) or  Matriz.objects.filter(column__exact=self.column):
+            raise ValidationError("")
+        else:
+            return True
+
 
     def save(self, *args, **kwargs):
-        self.element_is_within_limits()
-        super().save(*args, **kwargs)
+        if self.element_is_within_limits():
+            super().save(*args, **kwargs)
