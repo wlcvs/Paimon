fc91f92446ea92ddb7031be58fc8329c0fca2a57
Wallacy V. <wlcsv8080@outlook.com>
2024-12-23 17:48:06 -0300

    Tá tudo certo né

--- matriz/web/models.py
+++ matriz/web/models.py
@@ -18,14 +18,24 @@ class Element(models.Model):
         else:
             return True
 
+<<<<<<< HEAD
     # Verificar se o elemento já nesta posição já existe na matriz em questão
     def there_is_already_an_element_in_this_position(self):
         if  Matriz.objects.filter(line__exact=self.line) or  Matriz.objects.filter(column__exact=self.column):
             raise ValidationError(f"There is already an element at this position in the matriz, line: {self.line}, column: {self.column}")
         else:
+=======
+    # Verifica se não há nenhum elemento na posição em questão
+    def there_is_no_element_in_this_position_yet(self):
+        if !(Matriz.objects.filter(line__exact = self.line).exists()) and !(Matriz.objects.filter(column__exact = self.column).exists()):
+>>>>>>> 4104a10 (Pelo menos alguma coisa está sendo listada)
             return True
-
+        else:
+            raise ValidationError(f"There is already an element in row {self.line} and column {self.column}")
 
     def save(self, *args, **kwargs):
         if self.element_is_within_limits() and self.there_is_already_an_element_in_this_position():
             super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Value {self.value} in the matriz {self.matriz.name} in row {self.line} and column {self.column}"
--- matriz/web/views.py
+++ matriz/web/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
+from .models import Matriz
+
 def index(request):
-    return render(request, "web/index.html")
+    matrizes = Matriz.objects.all()
+    context = {
+        "matrizes": matrizes
+    }
+    return render(request, "web/index.html", context)
+
+def detail(request, pk):
+    matriz = Matriz.objects.get(pk = pk)
+    context = {
+        "matriz": matriz
+    }
+    return render(request, "web/detail.html", context)
